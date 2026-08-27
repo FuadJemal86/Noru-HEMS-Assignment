e63385d42d7756c560ac93e6f3970aca3922da47
Fuad Jemal <fuad47722@gmail.com>
2026-08-27 14:11:00 +0300

    fix: remove trailing slash from production API URL

--- server/index.js
+++ server/index.js
@@ -19,7 +19,7 @@ app.use(
         origin: [
             "http://localhost:5173",
             "http://localhost:5174",
-            "https://noru-hems-assignment.vercel.app/"
+            "https://noru-hems-assignment.vercel.app"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "UPDATE"],
         credentials: true,
