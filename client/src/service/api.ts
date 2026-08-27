f16c10697ae91e88d3ff975c34274ae5360df6b2
Fuad Jemal <fuad47722@gmail.com>
2026-08-27 14:04:56 +0300

    fix: update deployment server URL in API configuration

--- client/src/service/api.ts
+++ client/src/service/api.ts
@@ -3,12 +3,12 @@ import axios, { CanceledError } from "axios";
 const isDevelopment = import.meta.env.MODE === "development";
 const api = axios.create({
   baseURL: isDevelopment
-    ? "http://localhost:4000/api" : "DEPLOYMENT SERVER URL",
+    ? "http://localhost:4000/api" : "https://noru-hems-assignment.onrender.com",
   withCredentials: true,
 });
 
 const nPoint = isDevelopment
-  ? "http://localhost:4000/" : "DEPLOYMENT SERVER URL"
+  ? "http://localhost:4000/" : "https://noru-hems-assignment.onrender.com"
 
 export default api;
 export { CanceledError, nPoint };
